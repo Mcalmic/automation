aa4ac5dada520ee9fd08792f0c46391c885c7ab4
Mcalmic <69126774+Mcalmic@users.noreply.github.com>
2021-12-08 17:36:40 -0800

    Add files via upload

---
+++ autoclicker.py
@@ -0,0 +1,26 @@
+import pyautogui
+while 1 < 2:
+    answer = str(input("thingy"))
+    if answer == "st":
+        pyautogui.click(737, 478)
+    elif answer == "il":
+        pyautogui.click(675, 652)
+    elif answer == "ex":
+        pyautogui.click(750, 600)
+    elif answer == "qu":
+        pyautogui.click(765, 705)
+    elif answer == "ti":
+        pyautogui.click(668, 818)
+    elif answer == "bi":
+        pyautogui.click(820, 566)
+    elif answer == "re":
+        pyautogui.click(627, 599)
+    elif answer == "an":
+        pyautogui.click(616, 524)
+    elif answer == "fl":
+        pyautogui.click(856, 623)
+    elif answer == "sa":
+        pyautogui.click(685, 715)
+    elif answer == "pe":
+        pyautogui.click(745, 526)
+    
